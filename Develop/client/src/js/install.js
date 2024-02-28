fbe983e8c36fb14c615f023d23dee90b6833e1a9
song-richard <rsong1993@gmail.com>
2024-02-27 21:20:02 -0800

    updated butInstall with if statements to show window prompt, awaits users response, and checks users choice

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -12,7 +12,25 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (window.deferredPrompt) {
+        // Shows the installation prompt
+        window.deferredPrompt.prompt();
+        // Waits for the user to respond to the prompt
+        const choiceResult = await window.deferredPrompt.userChoice;
+        // Check the user's choice with if statement
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+        // Clears the deferredPrompt
+        window.deferredPrompt = null;
+        // Hide the install button
+        butInstall.style.display = 'none';
+      }
+});
+
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
